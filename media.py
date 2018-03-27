1e3bacd0ad2dca65d2cec8e0b15c9b8f2fd7f52f
Jinal Patel <jinalpatel1212@gmail.com>
2018-03-26 21:38:16 -0400

    formatted media file

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 class Movie():
-    " " "Creates Movies with title, imageUrl and trailerUrl. """
+    " " "Creates Movies with title, imageUrl and trailerUrl."""
 
-    def __init__ (self , title , postUrl , trailerUrl):
-    	" " "Inits Movie class with title , postUrl, trailerUrl. """
-    	self.title = title;
-    	self.poster_image_url = postUrl;
-    	self.trailer_youtube_url = trailerUrl;
+    def __init__(self, title, postUrl, trailerUrl):
+        " " "Inits Movie class with title, postUrl, trailerUrl."""
+        self.title = title
+        self.poster_image_url = postUrl
+        self.trailer_youtube_url = trailerUrl
